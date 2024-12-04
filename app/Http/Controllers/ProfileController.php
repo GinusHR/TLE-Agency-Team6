0a30230a8eeaac9e95028a4ecd489f691bb2b210
Irpear <145675108+Irpear@users.noreply.github.com>
2024-12-04 15:40:17 +0100

    email aanpassen WERKT (weer)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -52,13 +52,15 @@ class ProfileController extends Controller
     {
         $request->user()->fill($request->validated());
 
+
+
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
 
         $request->user()->save();
 
-        return Redirect::route('profile.show')->with('status', 'profile-updated');
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
     /**
