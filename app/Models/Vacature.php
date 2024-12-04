5cc34731a0d12dc11684e339d92064f78797c811
mickb <mickbreijer@outlook.com>
2024-12-04 10:05:22 +0100

    Reformated Vacature Model

--- app/Models/Vacature.php
+++ app/Models/Vacature.php
@@ -9,10 +9,17 @@ class Vacature extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['company_id', 'function', 'salary', 'workhours','place', 'location', 'time_id', 'description', 'secondary_info_needed', 'image', 'status'];
-
-    public function days()
-    {
-        return $this->belongsToMany(Day::class, 'day_vacature', 'vacature_id', 'day_id');
-    }
+    protected $fillable = [
+        'company_id',
+        'function',
+        'salary',
+        'workhours',
+        'place',
+        'location',
+        'time_id',
+        'description',
+        'secondary_info_needed',
+        'image',
+        'status'
+    ];
 }
