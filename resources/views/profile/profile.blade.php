7af4624f05beb7808b8bfab73085ade777d196dc
Irpear <145675108+Irpear@users.noreply.github.com>
2024-12-04 11:04:18 +0100

    Inloggen of registreren brengt je naar de profielpagina ipv het dashboard

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Profiel') }}
+                {{ __('Mijn profiel') }}
             </h2>
             <div class="flex gap-4">
                 <form method="POST" action="{{ route('logout') }}" class="inline">
