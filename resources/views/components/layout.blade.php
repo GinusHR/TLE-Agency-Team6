ab8b9ddf1797aecaf07505257aba52ce560bfea6
Irpear <145675108+Irpear@users.noreply.github.com>
2024-12-10 11:53:30 +0100

    rare zin aangepast 3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -67,7 +67,7 @@
         <div class="footer-links">
             <h3 class="h3-footer">Voor werkgevers</h3>
             <a class="nav-link-footer" href="/register">Werken met Open Hiring</a>
-            <a class="nav-link-footer" href="{{ route('company.dashboard') }}">Bedrijf Dashboard</a>
+            <a class="nav-link-footer" href="{{ route('company.dashboard') }}">Dashboard Bedrijf</a>
         </div>
 
         <div class="footer-links">
