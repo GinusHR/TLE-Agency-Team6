a8ecd1eb2916f57434709811603b42ea7c1d2734
Irpear <145675108+Irpear@users.noreply.github.com>
2024-12-10 14:28:06 +0100

    contactpagina toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ Route::get('/welcome', function () {
     return view('welcome');
 })->name('welcome');
 
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
 Route::get('/', [HomepageController::class, 'index'])->name('homepage');
 
 Route::get('/dashboard', function () {
