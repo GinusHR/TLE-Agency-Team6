cb1ad01a5ded925e53cf2db468506b52078c3f48
David de Knegt <daviddeknegt@gmail.com>
2024-12-11 15:37:26 +0100

    fixed footer mobile and desktop view

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,20 +15,20 @@
 
 <body class="bg-cream sm:max-w-full">
 
-<header class="header">
-    <a href="/">
-        <img src="{{ asset('images/logo.png') }}" alt="Header Afbeelding" class="w-[4vw] left-[2vw]">
-    </a>
-    @if (request()->is('company*'))
-    @else
-    <nav class="nav-bar max-w-full">
-        <!-- Dropdown Menu voor navigatie links -->
-        <div class="navbar-dropdown">
-            <button class="menu-icon">
-                <span class="block w-6 h-0.5 bg-moss-dark"></span>
-                <span class="block w-6 h-0.5 bg-moss-dark"></span>
-                <span class="block w-6 h-0.5 bg-moss-dark"></span>
-            </button>
+    <header class="header">
+        <a href="/">
+            <img src="{{ asset('images/logo.png') }}" alt="Header Afbeelding" class="w-[4vw] left-[2vw]">
+        </a>
+        @if (request()->is('company*'))
+        @else
+            <nav class="nav-bar max-w-full">
+                <!-- Dropdown Menu voor navigatie links -->
+                <div class="navbar-dropdown">
+                    <button class="menu-icon">
+                        <span class="block w-6 h-0.5 bg-moss-dark"></span>
+                        <span class="block w-6 h-0.5 bg-moss-dark"></span>
+                        <span class="block w-6 h-0.5 bg-moss-dark"></span>
+                    </button>
 
                     <!-- Dropdown content -->
                     <div class="dropdown-content">
@@ -58,39 +59,40 @@
     </main>
 
 
-<footer class="bg-moss-dark h-auto rounded-t-3xl max-w-full flex justify-center">
-    <div class="footer-content flex flex-wrap  sm:grid sm:grid-cols-2 sm:grid-rows-2 ">
-        <!-- Voor Werkzoekenden -->
-        <div class="footer-links">
-            <h3 class="h3-footer">Voor werkzoekenden</h3>
-            <a class="nav-link-footer" href="/register">Vind een baan</a>
-            <a class="nav-link-footer" href="/login">Veelgestelde vragen</a>
-        </div>
+    <footer class="bg-moss-dark h-auto rounded-t-3xl max-w-full flex justify-center">
+        <div class="footer-content flex flex-wrap grid grid-cols-2 grid-rows-2 ">
+            <!-- Voor Werkzoekenden -->
+            <div class="footer-links">
+                <h3 class="h3-footer">Voor werkzoekenden</h3>
+                <a class="nav-link-footer" href="/register">Vind een baan</a>
+                <a class="nav-link-footer" href="/login">Veelgestelde vragen</a>
+            </div>
 
-        <!-- Voor Werkgevers -->
-        <div class="footer-links">
-            <h3 class="h3-footer">Voor werkgevers</h3>
-            <a class="nav-link-footer" href="/register">Werken met Open Hiring</a>
-            <a class="nav-link-footer" href="{{ route('company.dashboard') }}">Dashboard Bedrijf</a>
-        </div>
+            <!-- Voor Werkgevers -->
+            <div class="footer-links">
+                <h3 class="h3-footer">Voor werkgevers</h3>
+                <a class="nav-link-footer" href="/register">Werken met Open Hiring</a>
+                <a class="nav-link-footer" href="{{ route('company.dashboard') }}">Dashboard Bedrijf</a>
+            </div>
 
-        <!-- Over Open Hiring -->
-        <div class="footer-links">
-            <h3 class="h3-footer">Over open hiring</h3>
-            <a class="nav-link-footer" href="/register">Onstaan</a>
-            <a class="nav-link-footer" href="/login">Privacy beleid</a>
-        </div>
+            <!-- Over Open Hiring -->
+            <div class="footer-links">
+                <h3 class="h3-footer">Over open hiring</h3>
+                <a class="nav-link-footer" href="/register">Onstaan</a>
+                <a class="nav-link-footer" href="/login">Privacy beleid</a>
+            </div>
 
-        <!-- Volg ons op -->
-        <div class="footer-links">
-            <h3 class="h3-footer">Volg ons op</h3>
-            <a class="nav-link-footer" href="https://www.linkedin.com/company/open-hiring-nl/"
-               target="_blank">Linkedin</a>
-            <a class="nav-link-footer" href="https://www.instagram.com/openhiring_nl" target="_blank">Instagram</a>
-            <a class="nav-link-footer" href="https://www.facebook.com/Openhiringnl" target="_blank">Facebook</a>
+            <!-- Volg ons op -->
+            <div class="footer-links">
+                <h3 class="h3-footer">Volg ons op</h3>
+                <a class="nav-link-footer" href="https://www.linkedin.com/company/open-hiring-nl/"
+                    target="_blank">Linkedin</a>
+                <a class="nav-link-footer" href="https://www.instagram.com/openhiring_nl" target="_blank">Instagram</a>
+                <a class="nav-link-footer" href="https://www.facebook.com/Openhiringnl" target="_blank">Facebook</a>
+            </div>
         </div>
-    </div>
-</footer>
+    </footer>
 
 </body>
+
 </html>
