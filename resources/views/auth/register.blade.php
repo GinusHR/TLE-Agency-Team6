53a8937a45199eca852dff497a6b2e0aa18f90bc
Irpear <145675108+Irpear@users.noreply.github.com>
2024-12-11 10:47:54 +0100

    inlog- en registratiepagina gestyled

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,40 +4,44 @@
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-label for="email" :value="__('Email')" class="text-lg sm:text-sm" />
+            <x-text-input id="email" class="block mt-1 w-full text-lg sm:text-sm" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Wachtwoord')" />
+            <x-input-label for="password" :value="__('Wachtwoord')" class="text-lg sm:text-sm" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full text-lg sm:text-sm"
+                          type="password"
+                          name="password"
+                          required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Bevestig wachtwoord')" />
+            <x-input-label for="password_confirmation" :value="__('Bevestig wachtwoord')" class="text-lg sm:text-sm" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full text-lg sm:text-sm"
+                          type="password"
+                          name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <div>
+            <p class="text-lg sm:text-xs mt-4">Bij Open Hiring nemen we jouw privacy serieus. Jouw gegevens worden strikt vertrouwelijk behandeld en nooit gedeeld met derden. Werkgevers hebben geen toegang tot jouw persoonlijke informatie. Je kunt je met een gerust hart registreren.</p>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            <a class="underline text-lg sm:text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Al geregistreerd?') }}
             </a>
 
-            <x-primary-button class="ms-4">
+            <x-primary-button class="ms-4 text-lg sm:text-xs">
                 {{ __('Registreer') }}
             </x-primary-button>
         </div>
