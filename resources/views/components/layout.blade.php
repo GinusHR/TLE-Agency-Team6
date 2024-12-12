a0864df37386a3a5bb09edd130d950052dd95615
David de Knegt <daviddeknegt@gmail.com>
2024-12-12 12:06:35 +0100

    removed mix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
     <title>Open Hiring</title>
     @vite('resources/js/app.js')
 </head>
