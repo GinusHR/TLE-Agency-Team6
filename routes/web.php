43519fde8a8e628cf7a81828ed3ed35cb973925f
David de Knegt <daviddeknegt@gmail.com>
2024-12-17 11:10:11 +0100

    added working datepicker in invitation page

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,7 @@ Route::prefix('company')->name('company.')->group(function () {
 Route::get('/invitations/{hash}/{invitation}', [InvitationController::class, 'show'])->name('invitations.show');
 Route::post('/invitations/{hash}/{invitation}/acceptInvitation', [InvitationController::class, 'acceptInvitation'])->name('invitations.acceptInvitation');
 Route::post('/invitations/{hash}/{invitation}/declineInvitation', [InvitationController::class, 'declineInvitation'])->name('invitations.declineInvitation');
+Route::post('/invitations/{hash}/{invitation}/changeInvitation', [InvitationController::class, 'changeInvitation'])->name('invitations.changeInvitation');
 
 
 Route::resource('vacatures', VacatureController::class);
